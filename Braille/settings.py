9d9781cc9ca7dc9634dcbce99b08358dfa574f52
Dani <daniela.lopez.araujo@gmail.com>
2024-07-23 19:46:35 -0600

    Some comments were removed

--- Braille/settings.py
+++ Braille/settings.py
@@ -16,11 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2#zg_dilv8-9@!y*_7ly4&dt0g8#logo!)v)=8@9y(=dvd+2)j'
+#SECRET_KEY = 'django-insecure-2#zg_dilv8-9@!y*_7ly4&dt0g8#logo!)v)=8@9y(=dvd+2)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
